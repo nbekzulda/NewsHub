ead232101f87f87eafc1c4e3a03aca134255c356
Nursultan Bekzulda <6279653-nbekzulda@users.noreply.gitlab.com>
2020-07-06 13:06:59 +0600

    prepared data

--- app/src/main/java/com/example/newshub/MainActivity.kt
+++ app/src/main/java/com/example/newshub/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.newshub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewPager: ViewPager
+    private lateinit var tabLayoutNews: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.viewPager_id)
+        tabLayoutNews = findViewById(R.id.tabNews)
+
+        val fragmentAdapter = MyPagerAdapter(supportFragmentManager)
+
+        viewPager.adapter = fragmentAdapter
+
+        tabLayoutNews.setupWithViewPager(viewPager)
+
     }
 }
